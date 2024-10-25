ddedcd042d37599cbb25ee9deb4601961c010380
Enrico Wiegmann <enrico.wiegmann@ssi-schaefer.com>
2024-10-25 11:02:28 +0200

    project before generics

--- polls/views.py
+++ polls/views.py
@@ -18,19 +18,22 @@ def index(request):
 
 def details(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    return render(request, "polls/details.html", {"question": question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s." % question_id
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {"question": question, "error_message": "Please select a choice"})
+        return render(request, 'polls/details.html', {"question": question, "error_message": "Please select a choice"})
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
     return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
